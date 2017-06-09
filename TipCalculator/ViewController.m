ab373ba38031d92bfdce58cc7fc7536e5e2014f7
Alex Lee <x.lee@mail.utoronto.ca>
2017-06-09 09:48:25 -0700

    Slider allows user to quickly adjust tip percentage (displayed next to the slider)

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -12,7 +12,9 @@
 
 @property (weak, nonatomic) IBOutlet UITextField *billAmountField;
 
-@property (weak, nonatomic) IBOutlet UITextField *tipField;
+@property (weak, nonatomic) IBOutlet UISlider *tipSlider;
+
+@property (weak, nonatomic) IBOutlet UILabel *tipPercentDisplay;
 
 @property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
 
@@ -34,10 +36,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)sliderValueChanged:(UISlider *)sender {
+    if (sender == self.tipSlider) {
+        self.tipPercentDisplay.text = [NSString stringWithFormat:@"%.0lf%%",floor(self.tipSlider.value)];
+    }
+}
+
 - (IBAction)calculateTip:(UIButton *)sender {
     
-    float tipRawNum = [self.tipField.text floatValue];
+    float tipRawNum = floor(self.tipSlider.value);
     float tipPercent = (tipRawNum == 0) ? 0.15 : tipRawNum / 100;
+    
     NSString *baseInput = self.billAmountField.text;
     NSNumber *baseCost = [NSNumber numberWithFloat:[baseInput floatValue]];
     
